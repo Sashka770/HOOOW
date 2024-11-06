4b51abbf77a5585fb9434ad66055c66c625042e0
Sashka770 <sashanabat2008@example.co>
2024-11-06 14:38:37 +0300

    dz

---
+++ 8dz.py
@@ -0,0 +1,14 @@
+
+obj = {
+     "key_num":50,
+     "num":0,
+     5:123,
+     27:"i string",
+     "30":[1,2,3],}
+obj_2={"car":53,
+     "num":228,
+     5:321,
+     27:"i slay",
+     "30":[28,4,52],}
+obj.update(obj_2)
+print(obj)
